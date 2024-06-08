f767b46c60474fbfa32802ba56d2b768e0b2818e
marcoxmediran <marcoxmediran@gmail.com>
2024-06-08 16:16:09 +0800

    removed dynamic_color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:databases_final_project/ui/home.dart';
 
@@ -12,21 +11,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Databases Final Project',
-        theme: ThemeData(
-          colorScheme: lightDynamic,
-          useMaterial3: true,
-        ),
-        darkTheme: ThemeData(
-          colorScheme: darkDynamic,
-          useMaterial3: true,
-        ),
-        home: const Home(),
-      );
-    });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Databases Final Project',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
   }
 }
